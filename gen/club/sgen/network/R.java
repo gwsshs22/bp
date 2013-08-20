ffb66599bc9fbcaf6f130f2cb7055180ec796179
maykim <lapajh2@hanmail.net>
2013-08-20 17:16:16 +0900

    drawable만 바꿈

--- gen/club/sgen/network/R.java
+++ gen/club/sgen/network/R.java
@@ -8,6 +8,16 @@
 package club.sgen.network;
 
 public final class R {
+    public static final class anim {
+        public static final int accelerate_interpolator=0x7f040000;
+        public static final int decelerate_interpolator=0x7f040001;
+        public static final int push_left_in=0x7f040002;
+        public static final int push_left_out=0x7f040003;
+        public static final int push_right_in=0x7f040004;
+        public static final int push_right_out=0x7f040005;
+        public static final int wallpaper_open_enter=0x7f040006;
+        public static final int wallpaper_open_exit=0x7f040007;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,29 +27,47 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int action_add_betting=0x7f020000;
+        public static final int action_search=0x7f020001;
+        public static final int betting_item=0x7f020002;
+        public static final int drawer_shadow=0x7f020003;
+        public static final int earth=0x7f020004;
+        public static final int friend=0x7f020005;
+        public static final int green=0x7f020006;
+        public static final int ic_drawer=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int jupiter=0x7f020009;
+        public static final int logout=0x7f02000a;
+        public static final int mars=0x7f02000b;
+        public static final int mercury=0x7f02000c;
+        public static final int my_page=0x7f02000d;
+        public static final int neptune=0x7f02000e;
+        public static final int saturn=0x7f02000f;
+        public static final int uranus=0x7f020010;
+        public static final int venus=0x7f020011;
+        public static final int white=0x7f020012;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000f;
-        public static final int betting_make_btn=0x7f080004;
-        public static final int btn_camera_crop=0x7f08000d;
-        public static final int btn_gellary_crop=0x7f08000e;
-        public static final int goal=0x7f080007;
-        public static final int id_text=0x7f080001;
-        public static final int image_down_test_btn=0x7f08000b;
-        public static final int image_test_delete_btn=0x7f08000c;
-        public static final int img_down_btn=0x7f080005;
-        public static final int m_number=0x7f080008;
-        public static final int password_text=0x7f080002;
-        public static final int profile_image=0x7f080006;
-        public static final int request_btn=0x7f080003;
-        public static final int upload_button=0x7f080009;
-        public static final int user_image=0x7f080000;
-        public static final int user_image_test=0x7f08000a;
+        public static final int action_settings=0x7f09000f;
+        public static final int betting_make_btn=0x7f090004;
+        public static final int btn_camera_crop=0x7f09000d;
+        public static final int btn_gellary_crop=0x7f09000e;
+        public static final int goal=0x7f090007;
+        public static final int id_text=0x7f090001;
+        public static final int image_down_test_btn=0x7f09000b;
+        public static final int image_test_delete_btn=0x7f09000c;
+        public static final int img_down_btn=0x7f090005;
+        public static final int m_number=0x7f090008;
+        public static final int password_text=0x7f090002;
+        public static final int profile_image=0x7f090006;
+        public static final int request_btn=0x7f090003;
+        public static final int upload_button=0x7f090009;
+        public static final int user_image=0x7f090000;
+        public static final int user_image_test=0x7f09000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -48,12 +76,12 @@ public final class R {
         public static final int image_crop_row=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -77,10 +105,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
